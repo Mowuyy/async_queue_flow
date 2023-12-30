6fb81b12d1d88948c210b42f91e85dfa6b45a27d
wu <wumo@zhiyi.ai>
2023-12-30 21:48:01 +0800

    添加代码

---
+++ async_queue_flow/__init__.py
@@ -0,0 +1,119 @@
+# -*- encoding: utf-8 -*-
+
+'''
+@Time        :   2023/12/30 19:52:10
+'''
+
+import asyncio
+import inspect
+from typing import Any, Callable
+from collections.abc import Coroutine
+
+AsyncCallable = Callable[..., ...]
+
+
+async def _producer(queue, tasks):
+    for idx in range(len(tasks)):
+        await queue.put((idx, tasks[idx], 0))
+
+
+async def _consumer(queue, result_queue, task_func, task_name, error_value=None, max_retry=3):
+    await asyncio.sleep(1)
+    print(f"consumer {task_name} start")
+
+    while True:
+        try:
+            idx, item, attempts = await asyncio.wait_for(queue.get(), timeout=10)
+            if item is None:
+                queue.task_done()
+                break
+            try:
+                result = await task_func(item)
+            except Exception as e:
+                if attempts > max_retry:
+                    print(f"consumer {task_name} error, qsize={queue.qsize()}, error_msg={e}")
+                    await result_queue.put((idx, error_value))
+                else:
+                    await queue.put((idx, item, attempts + 1))
+            else:
+                await result_queue.put((idx, result))
+            finally:
+                queue.task_done()
+        except asyncio.TimeoutError:
+            break
+
+
+async def _stop_signal(task_queue, consumer_size):
+    for _ in range(consumer_size):
+        await task_queue.put((0, None, 0))
+
+
+async def _parse_result(result_queue):
+    results = []
+    while not result_queue.empty():
+        result = await result_queue.get()
+        results.append(result)
+    results.sort(key=lambda x: x[0], reverse=False)
+    results = list(map(lambda x: x[1], results))
+    return results
+
+
+async def work_flow(
+    task_items: list,
+    task_func: AsyncCallable,
+    consumer_size: int = 5,
+    error_value: Any = None,
+    max_retry: int = 3
+):
+    if not inspect.iscoroutinefunction(task_func):
+        raise ValueError("task_func must be async function")
+    assert consumer_size > 1, "max_retry must be greater than 1"
+    assert max_retry > 0, "max_retry must be greater than 0"
+    
+    result_queue = asyncio.Queue()
+    task_queue = asyncio.Queue(
+        maxsize=len(task_items) * 1000  # 队列大小要远远大于（建议100倍）消费者数量，尤其是任务失败重试机制（当队列太小导致队列满时，重试没法推送任务到队列中，导致系统阻塞）
+    )
+
+    producers = asyncio.create_task(_producer(task_queue, task_items))
+    consumers = [
+        asyncio.create_task(
+            _consumer(
+                task_queue,
+                result_queue,
+                task_func,
+                task_name,
+                error_value=error_value,
+                max_retry=max_retry
+            )
+        )
+        for task_name in range(consumer_size)
+    ]
+
+    await producers
+    await task_queue.join()
+    await _stop_signal(task_queue, consumer_size)
+    await asyncio.gather(*consumers)
+    return await _parse_result(result_queue)
+
+
+if __name__ == "__main__":
+    import random
+    async def task_func(item):
+        if random.random() < 0.3:  # 假设有30%的失败概率
+            raise ValueError("任务处理失败")
+        await asyncio.sleep(0.5)
+        print("任务处理完成：", item)
+        return item
+
+    task_items = [f"test_{i}" for i in range(100)]
+    results = asyncio.run(
+        work_flow(
+            task_items,
+            task_func,
+            consumer_size=10,
+            error_value='Error',
+            max_retry=3
+        )
+    )
+    print("所有任务结果：", results)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='async_queue_flow',
+    version='0.1.0',
+    author='Morgan',
+    author_email='mowu_work@foxmail.com',
+    packages=find_packages(),
+    url='https://github.com/Mowuyy/async_queue_flow',
+    license='LICENSE',
+    description='An awesome async package for Python.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
